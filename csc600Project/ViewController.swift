1556e6b7259e41e6b659df6c76c10be1019fdd10
jinanh <jhuang27@mail.sfsu.edu>
2020-05-11 08:59:27 -0700

    initial commit

---
+++ csc600Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  csc600Project
+//
+//  Created by Jinan Huang on 5/11/20.
+//  Copyright © 2020 Lum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
